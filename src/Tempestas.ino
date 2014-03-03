b6593070af267b1d115b5681801365bace4fe4d5
John Burrin <jonnyb3010@gmail.com>
2014-03-03 19:52:53 +0000

    Commit BMP085

--- src/Tempestas.ino
+++ src/Tempestas.ino
@@ -2,18 +2,20 @@
 #include <DallasTemperature.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <Wire.h>
 #include <DHT.h>
-#inliude "bmp085_functions.cpp"
+
+#include "bmp085_functions.h"
+
 #define ONE_WIRE_BUS 10
 #define DHT_PIN 2
 
 #define TEMPERATURE_PRECISION 12
 #define MAX_DEVICES 5
 
-
 int getTemperatureAsInt(DeviceAddress deviceAddress);
-void pipe2Pi(int [], float, float);
+void pipe2Pi(int ptrData[], float dht_temp, float humidity, long pressure, float altitude, short bmp085temperature);
+//void pipe2Pi(int [], float, float);
 float readDHT_Humidity(void);
 float readDHT_Temperature(void);
 
@@ -32,7 +34,10 @@ void setup() {
   // initialize inputs/outputs
   // start serial port
   Serial.begin(9600);
-  
+  Wire.begin();
+  bmp085Calibration();
+
+
   // Start up the libraries
   dht.setup(DHT_PIN); // data pin 2
   sensors.begin();
@@ -45,6 +50,10 @@ void setup() {
 }
 
 void loop() {
+  temperature = bmp085GetTemperature(bmp085ReadUT());
+  pressure = bmp085GetPressure(bmp085ReadUP());
+  altitude = (float)44330 * (1 - pow(((float) pressure/p0), 0.190295));
+
   int dsData[deviceCount];
   int t;
   //Serial.println("Incomming...");
@@ -57,11 +66,11 @@ void loop() {
   float humidity = readDHT_Humidity();
   float dht_temperature = readDHT_Temperature();
   // Now push everything to output
-  pipe2Pi(dsData, dht_temperature, humidity);
+  pipe2Pi(dsData, dht_temperature, humidity, pressure,altitude,temperature);
 }
 
 
-void pipe2Pi(int ptrData[], float dht_temp, float humidity){
+void pipe2Pi(int ptrData[], float dht_temp, float humidity, long pressure, float altitude, short bmp085temperature){
   String output;
   // Squirt all the data to the Pi
   Serial.print( (float) (ptrData[0] * 0.0078125) );
@@ -73,6 +82,12 @@ void pipe2Pi(int ptrData[], float dht_temp, float humidity){
   Serial.print ((float) dht_temp);
   Serial.print (",");
   Serial.print ((float) humidity);
+  Serial.print (",");
+  Serial.print ((long) pressure);
+  Serial.print (",");
+  Serial.print ((float) altitude);
+  Serial.print (",");
+  Serial.print ((short) bmp085temperature);
   Serial.println("\n");
   Serial.flush();
   delay(1000);
